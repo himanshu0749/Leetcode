92c42df63f0fa06b0ccea8be1316f0cdc2a56273
Himanshu Bhadoria <136106889+himanshu0749@users.noreply.github.com>
2024-07-31 18:33:12 +0530

    Create 112. Path Sum.java

---
+++ 112. Path Sum.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null) return false;
+        targetSum=targetSum-root.val;
+        if(targetSum==0 && root.left==null && root.right==null) return true;
+        return hasPathSum(root.left,targetSum) || hasPathSum(root.right,targetSum);
+    }
+}
